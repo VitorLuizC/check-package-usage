f8e1d3a27aa814345ce93403fba3c2602a2ad80a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2021-04-17 23:59:14 -0300

    🎨 improve function names and add JSDocs

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,34 @@ import type { Dirent } from 'fs';
 import * as path from 'path';
 import getDirents from './getDirents';
 
-function displayDirent(parent: string, dirent: Dirent): string {
+/**
+ * Formats received dirent to display in the corrent way.
+ * @param {string} parent
+ * @param {import('fs').Dirent} dirent
+ * @returns {string}
+ */
+function formatDirent(parent: string, dirent: Dirent): string {
   return path.resolve(parent, dirent.name) + (dirent.isDirectory() ? '/' : '');
+}
+
+/**
+ * Resolve path with received segments.
+ * @param {...string} segments
+ * @returns {string}
+ */
+function resolvePath(...segments: string[]): string {
+  return path.resolve(process.cwd(), ...segments);
 }
 
 const parent = '.';
 
-getDirents(path.resolve(parent))
+getDirents(resolvePath(parent))
   .then((dirents) => {
     process.stdout.write('List of files and directories: \n');
     dirents.forEach((dirent, index) => {
-      const number = (index + 1)
-        .toString(10)
-        .padStart(dirents.length.toString(10).length, ' ');
-      process.stdout.write(`${number}. ${displayDirent(parent, dirent)}\n`);
+      const maxLength = dirents.length.toString(10).length;
+      const number = (index + 1).toString(10).padStart(maxLength, ' ');
+      process.stdout.write(`${number}. ${formatDirent(parent, dirent)}\n`);
     });
   })
   .catch((error) => {
